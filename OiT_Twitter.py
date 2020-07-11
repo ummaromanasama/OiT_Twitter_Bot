288e7979c288f5a4fdadd3f1c73d114ed4c5768b
Ummaromana Sama <ummaromanasama@gmail.com>
2020-07-10 22:49:57 -0400

    Add files via upload

---
+++ OiT_Twitter.py
@@ -0,0 +1,37 @@
+import tweepy
+import time
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
+auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+
+#This will make the rest of the code obey the rate limit
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+user = api.me()
+
+search_1 = ['@outintech']
+
+nrTweets = 5
+
+for tweet in tweepy.Cursor(api.search, search_1).items(nrTweets):
+    try:
+        print('Liked')
+        tweet.favorite()
+        time.sleep(20)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+search_2 = ['#outintech']
+
+for tweet in tweepy.Cursor(api.search, search_2).items(nrTweets):
+    try:
+        print('Hearted')
+        tweet.favorite()
+        time.sleep(20)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
