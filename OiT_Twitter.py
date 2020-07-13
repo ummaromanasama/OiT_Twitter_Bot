75819b6302c5e1f886215e249d7f52d72bdd1766
Ummaromana Sama <ummaromanasama@gmail.com>
2020-07-13 13:33:42 -0400

    Update OiT_Twitter.py

--- OiT_Twitter.py
+++ OiT_Twitter.py
@@ -16,12 +16,17 @@ nrTweets = 5
 
 search = ['outintech'] # Phrase to search for
 
+# Message to reply to tweets
+message = 'Hey! @OutInTech has a Slack channel with over 13,000 LGBTQ+ professionals worldwide. Thought you might be interested in joining our active & inclusive Slack channel + meet new friends & supporters around the world. bit.ly/Slack-OIT'
+
 # Searching for phrase amongst tweets
 for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
     try:
-        print('Favorited')
+        api.update_status(status = message, in_reply_to_status_id = tweet.id_str)
+        print('Commented') # Indicates tweet is replied to
         tweet.favorite()
-        time.sleep(20)
+        print('Favorited') # Indicates tweet is favorited
+        time.sleep(20) # Mini break
     except tweepy.TweepError as e:
         print(e.reason)
     except StopIteration:
