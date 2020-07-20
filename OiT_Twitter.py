6730f99bbc5e8ff3951d0d2914d7497a47291d78
Ummaromana Sama <ummaromanasama@gmail.com>
2020-07-20 11:22:42 -0400

    Update OiT_Twitter.py

--- OiT_Twitter.py
+++ OiT_Twitter.py
@@ -12,21 +12,21 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 user = api.me()
 
 # Number of tweets to like
-nrTweets = 5
+nrTweets = 10
 
-search = ['outintech'] # Phrase to search for
+search = 'outintech' # Phrase to search for
 
-# Message to reply to tweets
-message = 'Hey! @OutInTech has a Slack channel with over 13,000 LGBTQ+ professionals worldwide. Thought you might be interested in joining our active & inclusive Slack channel + meet new friends & supporters around the world. bit.ly/Slack-OIT'
+# Comment on tweets
+comment = 'Hey! @OutInTech has a Slack channel with over 13,000 LGBTQ+ professionals worldwide. Thought you might be interested in joining our active & inclusive Slack channel + meet new friends & supporters around the world. bit.ly/Slack-OIT'
 
-# Searching for phrase amongst tweets
+# Searching for phrase amongst tweets and commenting on them
 for tweet in tweepy.Cursor(api.search, search).items(nrTweets):
     try:
-        api.update_status(status = message, in_reply_to_status_id = tweet.id_str)
-        print('Commented') # Indicates tweet is replied to
+        print('Tweet Liked')
         tweet.favorite()
-        print('Favorited') # Indicates tweet is favorited
-        time.sleep(20) # Mini break
+        print('Commented')
+        api.update_status(comment, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True)        
+        time.sleep(5)
     except tweepy.TweepError as e:
         print(e.reason)
     except StopIteration:
